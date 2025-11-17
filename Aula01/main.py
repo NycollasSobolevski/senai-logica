b74b264a966c5c3e1a13b3bb271dca2a42e7f1e0
nycollassobolevski <sobolevski.academy@gmail.com>
2025-11-17 19:39:29 -0300

    refatore aula 2

---
+++ Aula01/main.py
@@ -0,0 +1,61 @@
+#variavel = 'Meu texto' #string
+#variavel2 = 123 #int (inteiro)
+#variavel3 = 1.23 #float (com numeros decimais)
+#variavel4 = False #Boleano (verdadeiro/falso)
+
+# marca = "BMW"
+# veiculo = "320i"
+# print(marca + " " + veiculo)
+
+# a = 10
+# b = 17
+# c = a + b
+# print(c)
+
+# v = True
+# f = False
+# print(v)
+# print(f)
+# print(v == f)
+
+#------------------------------------------------------
+import math
+help(math) #mostra os comandos dentro de math
+math.cos(21)
+
+
+# --------------------------------------------------------
+# a = 1
+# b = 1.0
+# c = True
+# d = 4+3j
+
+# print(type(a))
+# print(type(b))
+# print(type(c))
+# print(type(d))
+
+# ----------------------------------------------
+# atalho para o terminal = ctrl + '
+# nome = input("Insira seu nome: ")
+# print("Seu nome é: ", nome)
+
+# -----------------------------
+# a = int(input("insira o valor de a:"))   #1
+# b = input('insira o valor de b:') #"1"
+# divisao = b + a
+
+# print("A resposta e:", divisao)
+
+# ---------------------------------
+# Conversao para fahrenheit
+# c = float(input("insira o valor: "))
+# f = (c * 9)/5 + 32 
+# print("Valor em fahrenheit: " +  str(f)  + "°F")
+
+# ---------------------------------
+
+tabela = "---------------" \
+"col1 col2 col3" \
+"it1  it2  it3 " \
+"---------------"
