b4d66437304570cfc023a2f96393d73718d04470
nycollassobolevski <sobolevski.academy@gmail.com>
2025-11-24 19:47:07 -0300

    add exemplos de condicionais

---
+++ Aula07/main.py
@@ -0,0 +1,30 @@
+# Condicionais e LOOPS
+
+
+# 1- Condicionais
+
+# EXEMPLO AND
+# Quando eu preciso fazer um pure de batatas, eu preciso
+# descascar batatas e para isso eu preciso de Batata E Faca
+
+# batata = True
+# faca = False 
+
+# if batata == True and faca == True:
+#     print("E possivel fazer um pure!!")
+# else: 
+#     print("Não é possivel fazer um pure!")
+
+# EXEMPLO OR
+# eu preciso comprar batatas, e preciso ter qualquer meio
+# de pagamento (debito, credito, pix, dinheiro)
+
+dinheiro = True
+credito = False
+pix = True
+debito = True
+
+if dinheiro == True or credito == True or pix == True or debito == True:
+    print("É possivel comprar batatas")
+else: 
+    print("nao tenho nenhum meio de pagar as batatas!")
