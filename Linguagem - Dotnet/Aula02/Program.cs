f29a6dea689732754ade22413d721cb142933cab
nycollassobolevski <sobolevski.academy@gmail.com>
2025-12-10 19:13:44 -0300

    update: antes de entrar em poo

---
+++ Linguagem - Dotnet/Aula02/Program.cs
@@ -0,0 +1,39 @@
+﻿// int idade = 19;
+
+// switch (idade)
+// {
+//     case 18:
+//         System.Console.WriteLine("Agora é maior de idade");
+//         break;
+//     case 19:
+
+//         System.Console.WriteLine("Tá ficando veio");
+//         goto case 18;
+
+// }
+
+
+
+// Exemplo for:
+
+// for (int i = 2; i < 10; i += 2)
+// {
+//     System.Console.WriteLine(i);
+// }
+
+
+// Exemplo funções 
+
+// calculo de módulo:
+
+int valor = Modulo(-3);
+System.Console.WriteLine(valor);
+
+int Modulo(int numero)  
+{
+    if (numero < 0)
+    {
+        numero = -numero;
+    }
+    return numero;
+}
---
+++ prova/main.py
@@ -0,0 +1,122 @@
+list = [
+    {
+        'coluna': 'CP',
+        'linha': 4,
+        'letra': 'l'
+    },
+    {
+        'coluna': 'CQ',
+        'linha': 4,
+        'letra': 'z'
+    },
+    {
+        'coluna': 'BC',
+        'linha': 4,
+        'letra': 'f'
+    },
+    {
+        'coluna': 'CP',
+        'linha': 4,
+        'letra': 'i'
+    },
+    {
+        'coluna': 'EH',
+        'linha': 4,
+        'letra': 'a'
+    },
+    {
+        'coluna': 'DF',
+        'linha': 4,
+        'letra': ' '
+    },
+    {
+        'coluna': 'CN',
+        'linha': 4,
+        'letra': 'e'
+    },
+    {
+        'coluna': 'EI',
+        'linha': 4,
+        'letra': 't'
+    },
+    {
+        'coluna': 'DG',
+        'linha': 4,
+        'letra': 'n'
+    },
+    {
+        'coluna': 'EK',
+        'linha': 4,
+        'letra': 'l'
+    },
+    {
+        'coluna': 'EJ',
+        'linha': 4,
+        'letra': 'a'
+    },
+    {
+        'coluna': 'A',
+        'linha': 5,
+        'letra': '\n'
+    },
+    {
+        'coluna': 'GF',
+        'linha': 6,
+        'letra': 's'
+    },
+    {
+        'coluna': 'FF',
+        'linha': 6,
+        'letra': 's'
+    },
+    {
+        'coluna': 'FG',
+        'linha': 6,
+        'letra': 't'
+    },
+    {
+        'coluna': 'FH',
+        'linha': 6,
+        'letra': 'a'
+    },
+    {
+        'coluna': 'DG',
+        'linha': 6,
+        'letra': 'o'
+    },
+    {
+        'coluna': 'EJ',
+        'linha': 6,
+        'letra': ' '
+    },
+    {
+        'coluna': 'FE',
+        'linha': 6,
+        'letra': 'e'
+    },
+    {
+        'coluna': 'DF',
+        'linha': 6,
+        'letra': 'b'
+    },
+    {
+        'coluna': 'EH',
+        'linha': 6,
+        'letra': 'a'
+    },
+    {
+        'coluna': 'EK',
+        'linha': 6,
+        'letra': 'f'
+    },
+    {
+        'coluna': 'EI',
+        'linha': 6,
+        'letra': 's'
+    },
+    {
+        'coluna': '',
+        'linha': 7,
+        'letra': '!'
+    },
+]
